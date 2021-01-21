205d887ca5faa0be06c8779b80b3febc669c112a
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-21 17:04:13 -0300

    criado os eventos para fechar e abrir os menus do attach

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -69,35 +69,112 @@ class WhatsAppController
         Element.prototype.hasClass = function(name) {
             return this.classList.contains(name);
         }
+
+        HTMLFormElement.prototype.getForm = function() {
+            return new FormData(this);
+        }
+
+        HTMLFormElement.prototype.toJson = function() {
+            
+            let json = {};
+
+            this.getForm().forEach((value, key) => {
+                json[key] = value;
+            });
+
+            return json;
+        }
+
+
     }
 
     initEvents()
     {
-        this.el.myPhoto.on('click', e => {
+        this.el.myPhoto.on('click', () => {
             this.closeAllLeftPanel();
             this.el.panelEditProfile.show();
+
             setTimeout(() => {
                 this.el.panelEditProfile.addClass('open');
             }, 10);
         });
 
-        this.el.btnNewContact.on('click', e => {
+        this.el.btnNewContact.on('click', () => {
             this.closeAllLeftPanel();
             this.el.panelAddContact.show();
+
             setTimeout(() => {
                 this.el.panelAddContact.addClass('open');
             }, 10);
         });
 
-        this.el.btnClosePanelEditProfile.on('click', e => {
+        this.el.btnClosePanelEditProfile.on('click', () => {
             this.el.panelEditProfile.removeClass('open');
         });
 
-        this.el.btnClosePanelAddContact.on('click', e => {
+        this.el.btnClosePanelAddContact.on('click', () => {
             this.el.panelAddContact.removeClass('open');
+        });
+
+        this.el.photoContainerEditProfile.on('click', e => {
+            this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+            if(e.key === 'Enter')
+            {
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+            console.log(this.el.inputNamePanelEditProfile.textContent);
+        });
+
+        this.el.formPanelAddContact.on('submit', e => {
+            e.preventDefault();
+
+            let formData = new FormData(this.el.formPanelAddContact);
+        });
+
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
+            item.on('click', e => {
+                this.el.home.hide();
+                this.el.main.css({
+                    display: 'flex'
+                });
+            });
+        });
+
+        this.el.btnAttach.on('click', e => {
+            e.stopPropagation();
+            this.el.menuAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+        });
+
+        this.el.btnAttachPhoto.on('click', () => {
+            console.log('photo')
+        });
+
+        this.el.btnAttachCamera.on('click', () => {
+            console.log('camera');
+        });
+
+        this.el.btnAttachDocument.on('click', () => {
+            console.log('document');
+        });
+
+        this.el.btnAttachContact.on('click', () => {
+            console.log('contact');
         });
     }
 
+    closeMenuAttach(e)
+    {
+        document.removeEventListener('click', this.closeMenuAttach);
+        this.el.menuAttach.removeClass('open');
+    }
     closeAllLeftPanel()
     {
         this.el.panelAddContact.hide();
