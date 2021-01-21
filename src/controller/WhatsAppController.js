0097af2fbe0ec6ed4db7d778475b6ec0b10f5a6c
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-21 15:38:22 -0300

    Criado os eventos nos botões de adicionar contato e atualizar perfil

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController
     {
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
     }
 
     loadElements()
@@ -68,5 +69,38 @@ class WhatsAppController
         Element.prototype.hasClass = function(name) {
             return this.classList.contains(name);
         }
+    }
+
+    initEvents()
+    {
+        this.el.myPhoto.on('click', e => {
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+            }, 10);
+        });
+
+        this.el.btnNewContact.on('click', e => {
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+            }, 10);
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+            this.el.panelEditProfile.removeClass('open');
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+            this.el.panelAddContact.removeClass('open');
+        });
+    }
+
+    closeAllLeftPanel()
+    {
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
     }
 }
