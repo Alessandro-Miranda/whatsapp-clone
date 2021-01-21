7cdea879ea1545024847e586c7d3b1718eb2f3c1
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-01-21 09:40:35 -0300

    Criado os prototypes para auxiliar na modificação de css

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -2,15 +2,71 @@ class WhatsAppController
 {
     constructor()
     {
+        this.elementsPrototype();
         this.loadElements();
     }
 
     loadElements()
     {
         this.el = {};
-
+        // pega todos os elementos que possuem id e alimenta o objeto com a referencia do elemento
         document.querySelectorAll('[id]').forEach(element => {
             this.el[Format.getCamelCase(element.id)] = element;
         });
+    }
+
+    elementsPrototype()
+    {
+        // Cria o prototipe encapsulando o contexto this
+        Element.prototype.hide = function() {
+            this.style.display = 'none';
+            return this;
+        }
+
+        Element.prototype.show = function() {
+            this.style.display = 'block';
+            return this;
+        }
+
+        Element.prototype.toggle = function() {
+            this.style.display = this.style.display === 'none' ? 'block' : 'none';
+            return this;
+        }
+
+        // melhorar está função para receber diversos eventos junto com funções diferentes
+        Element.prototype.on = function(events, fn) {
+            events.split(' ').forEach(event => {
+                // esse this se refere ao elemento do prototype. Como ele está dentro de uma arrow function, ele não tem escopo
+                this.addEventListener(event, fn);
+            });
+            return this;
+        }
+
+        Element.prototype.css = function(styles) {
+            for(var name in styles)
+            {
+                this.style[name] = styles[name];
+            }
+            return this;
+        }
+
+        Element.prototype.addClass = function(name) {
+            this.classList.add(name);
+            return this;
+        }
+
+        Element.prototype.removeClass = function(name) {
+            this.classList.remove(name);
+            return this;
+        }
+
+        Element.prototype.toggleClass = function(name) {
+            this.classList.remove(name);
+            return this;
+        }
+
+        Element.prototype.hasClass = function(name) {
+            return this.classList.contains(name);
+        }
     }
 }
--- src/utils/Format.js
+++ src/utils/Format.js
@@ -1,11 +1,11 @@
 class Format
 {
     static getCamelCase(text)
-    {
+    {   
         let div = document.createElement('div');
-
+        // cria uma div para poder utilizar o dataset que faz a transformação do id utilizando o dataset
         div.innerHTML = `<div data-${text}="id"></div>`;
-
+        // retorna a chave do elemento da div (id no padrão camel case)
         return Object.keys(div.firstChild.dataset)[0];
     }
 }
