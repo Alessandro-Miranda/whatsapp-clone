0d1990d7facef983bc97dd8603f305f3df55fc0a
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-06-23 11:47:40 -0300

    Criado o método para fechar a camera

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -1,4 +1,4 @@
-class CameraController
+export default class CameraController
 {
     constructor(videoEl)
     {
@@ -7,10 +7,18 @@ class CameraController
         navigator.mediaDevices.getUserMedia({
             video: true
         }).then(stream => {
+            this._stream = stream;
             this._videoEl.src = URL.createObjectURL(stream);
             this._videoEl.play();
         }).catch(err => {
             console.error(err);
+        });
+    }
+
+    stop()
+    {
+        stream.getTracks().forEach(track => {
+            track.stop();
         });
     }
 }
