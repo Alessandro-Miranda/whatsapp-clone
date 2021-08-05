972a92bdd4cb2bf947b6c65dd019746debdefde9
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-08-05 07:58:02 -0300

    Iniciado o tratamento de pré-visualização dos pdf

--- src/controller/DocumentPreviewController.js
+++ src/controller/DocumentPreviewController.js
@@ -1,3 +1,7 @@
+const pdfjslib = require('pdfjs-dist');
+
+pdfjslib.GlobalWorkerOptions.workerSrc = '../../node_modules/pdfjs-dist/build/pdf.worker.js';
+
 export default class DocumentPreviewController
 {
     constructor(file)
@@ -8,13 +12,14 @@ export default class DocumentPreviewController
     getPreviewData()
     {
         return new Promise((resolve, reject) => {
+            let reader = new FileReader();
+
             switch(this._file.type)
             {
                 case 'image/png':
                 case 'image/jpg':
                 case 'image/jpeg':
                 case 'image/gif':
-                    let reader = new FileReader();
                     reader.onload = e => {
                         resolve({
                             src: reader.result,
@@ -31,6 +36,16 @@ export default class DocumentPreviewController
 
                 case 'application/pdf':
 
+                    reader.onload = e => {
+                        pdfjslib.getDocument(new Uint8Array(reader.result)).promise.then(pdf => {
+                            console.log(pdf);
+                            resolve();
+                        }).catch(err => {
+                            reject(err);
+                        });
+                    }
+
+                    reader.readAsArrayBuffer(this._file);
                 break;
 
                 default:
