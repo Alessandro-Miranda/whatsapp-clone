6ed096de22040c1302926be7b32d67755b23a237
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-09-27 17:21:45 -0300

    Adicionado a clausula where

--- src/model/User.js
+++ src/model/User.js
@@ -61,10 +61,10 @@ export default class User extends Model
             .set(contact.toJSON());
     }
 
-    getContacts()
+    getContacts(filter = '')
     {
         return new Promise((resolve, reject) => {
-            User.getContactsRef(this.email).onSnapshot(docs => {
+            User.getContactsRef(this.email).where('name', '>=', filter).onSnapshot(docs => {
                 let contacts = [];
 
                 docs.forEach(doc => {
